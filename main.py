c9b3494f3d1f5e5f26180e5cf7719fe54d1a2f01
AdanGuzmanBiO <adan.guzman@barresiones.cat>
2026-01-20 09:53:38 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# *** LListes python ***
+
+def comprobarInt():
+    try:
+        return int(input("\nEscull l'opció que vols: "))
+
+    except ValueError:
+        print("Valor incorrecte")
+
+def comprobarIndexLlista(llista):
+    try:
+        return int(input("Escull el teu index: "))
+
+    except IndexError:
+        print("Fora de rang")
+
+alumne_nou = []
+
+alumnes = [
+    ["Anna", "Pujol", "SMX", "1r", "A101"],
+    ["Marc","Serra","SMX","2n","A102"],
+    ["Laia","Rovira","ASIX","1r","B201"],
+    ["Jordi","Casals","ASIX","2n","B202"]
+]
+
+while True:
+
+    print("\nLes opciones que pots triar son: ")
+    print("1. Mostra les dades de tots els alumnes")
+
+    opcio = comprobarInt()
+    match opcio:
+        case 1:
+            for i, alumne in enumerate(alumnes):
+                print(f"\nAlumne {i}:")
+                for j, campo in enumerate (alumne):
+                    print (f"Campo {j}:", campo)
+
+
+        case 2:
+            print("Opció 2")
+
+        
+        case 3:
+            print("Opció 3")
+
+        case 4:
+            print("Opció 4")
+
+        case 5:
+            print("Opció 5")
+
+        case 6:
+            print("Opció 6")
+            break
+
+        case _:
+            print("Opció incorrecta, tria una opció dins del rang")
