ddf29adffbd5377e288a5b4e0a6695063dd6ae90
AdanGuzmanBiO <adan.guzman@barresiones.cat>
2026-01-21 10:59:06 +0100

    commit tres

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def comprobarInt():
     except ValueError:
         print("Valor incorrecte")
 
-def comprobarStr():
+def comprobarStr(dada):
     try:
-        return input()
+        text = str(input(f"introdueix la dada {dada}: "))
         
     except ValueError:
         print ("Valor incorrecte")
+
+    return text
+
+
 
 def comprobarIndexLlista(llista):
     try:
@@ -47,9 +51,40 @@ while True:
 
         case 2:
             print("Dades del nou alumne: ")
-            nom = None
-            print(f"\nIndica el nom que vols: {nom = comprobarStr}")
+            dada = "nom "
+            nom = comprobarStr(dada)
+
+            dada = "cognom "
+            cognom = comprobarStr(dada)
             
+            dada = "grau "
+            grau = comprobarStr(dada)
+            
+            dada = "curs "
+            curs = comprobarStr(dada)
+
+            dada = "aula "
+            aula = comprobarStr(dada)
+
+            print("\nLes dades de l'alumne nou son: ")
+            print(f"""Nom: {nom}
+Cognom: {cognom}
+Grau: {grau}
+Curs: {curs}
+Aula: {aula}
+            """)
+
+            alumne_nou.append({
+                "nom" : nom,
+                "cognom" : cognom,
+                "grau" : grau,
+                "curs" : curs,
+                "aula" : aula
+            })
+
+            print(alumne_nou)
+
+            alumnes.append(alumne_nou)
 
         
         case 3:
