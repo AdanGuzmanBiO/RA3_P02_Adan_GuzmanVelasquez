018fe43ea786e053b16306f86aa11e7b85ac067f
AdanGuzmanBiO <adan.guzman@barresiones.cat>
2026-01-26 18:10:02 +0100

    commit cinc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # *** LListes python ***
 
+# Funcio que comprova que l'entrada sigui un enter y personalització de text
 def comprobarInt(text):
     try:
         return int(input(f"{text}"))
@@ -7,6 +8,7 @@ def comprobarInt(text):
     except ValueError:
         print("Valor incorrecte")
 
+# Funció per variar les dades que es demanen a l'usuari
 def comprobarStr(dada):
     try:
         text = str(input(f"introdueix la dada {dada}: "))
@@ -16,19 +18,22 @@ def comprobarStr(dada):
 
     return text
 
+# Funcio per veure la llista d'alumnes
 def LlistaAlumnes(alumnes):
     for i, alumne in enumerate (alumnes):
         print(f"\nL'alumne amb index {i} es")
         for j, campo in enumerate (alumne):
             print(f"Campo {j}: {campo}")
 
+# Funció per veure els alumnes d'un grau concret
 def GrauAlumne(alumnes_grau,alumnes):
     for i, alumne in enumerate (alumnes):
         for j, campo in enumerate (alumne):
             if alumnes_grau == campo.lower():
                 print(f"L'alumne {alumne[0]} cursa el grau {alumnes_grau}")
-                
-def comprobarIndexLlista(alumnes, text):
+
+# Funcio per comprovar l'index d'una llista y personalització de text
+def comprovarIndexLlista(comprovar_index, text):
     while True:
         try:
             while True:
@@ -37,7 +42,7 @@ def comprobarIndexLlista(alumnes, text):
                     print("L'index ha de ser un enter positiu")
                 else:
                     break
-            alumnes[index]
+            comprovar_index[index]
             return index
         
         except ValueError:
@@ -46,7 +51,7 @@ def comprobarIndexLlista(alumnes, text):
         except IndexError:
             print("Fora de rang, torna-ho a provar")
     
-
+#Dades inicials
 alumne_nou = []
 
 alumnes = [
@@ -56,6 +61,7 @@ alumnes = [
     ["Jordi","Casals","ASIX","2n","B202"]
 ]
 
+# Menu principal loopeable fins que l'usuari decideixi sortir
 while True:
 
     print("\nLes opciones que pots triar son: ")
@@ -68,9 +74,12 @@ while True:
 
     opcio_menu_01 = comprobarInt(text = "\nTria una opció del menú: ")
     match opcio_menu_01:
+
+        # Imprimeix la llista dels alumnes en pantalla
         case 1:
             LlistaAlumnes(alumnes)
 
+        # Agrega un nou alumne a la llista demanant les dades
         case 2:
             print("Dades del nou alumne: ")
             nom = comprobarStr(dada = "nom")
@@ -89,11 +98,14 @@ Aula: {aula}""")
             alumne_nou = [nom, cognom, grau, curs, aula]
             alumnes.append(alumne_nou)
         
+        # Elimina un alumne de la llista segons l'index
         case 3:
             print("\nLlista d'alumnes actual: ")
             LlistaAlumnes(alumnes)
-            del alumnes[comprobarIndexLlista(alumnes, text = "\nTria l'index de l'alumne que vols eliminar: ")]
+            del alumnes[comprovarIndexLlista(comprovar_index = alumnes, text = "\nTria l'index de l'alumne que vols eliminar: ")]
+            print("Alumne eliminat correctament")
 
+        # Mostra els alumnes d'un grau concret, demanant a l'usuari el grau que vol veure
         case 4:
             while True:
                 alumnes_grau = input("\nEls alumnes de qué grau vols veure(SMX/ASIX)? ").lower()
@@ -103,77 +115,90 @@ Aula: {aula}""")
                     print("Grau incorrecte, torna-ho a provar")
             
             GrauAlumne(alumnes_grau, alumnes)
-                
+        
+        # Menú per a que l'usuari pugui decidir quin tipus de modificació vol fer
         case 5:
-            print("Les opcions que pots triar son: ")
-            print("""1. Agregar una dada nova
-2. Eliminar una dada""")
-            opcio_menu_02 = comprobarInt(text = "\nTria entre agregar o eliminar una dada d'un alumne: ")
+            while True:
+                print("\nLes opcions que pots triar son: ")
+                print("""1. Agregar una dada nova
+2. Eliminar una dada
+3. Tornar al menú principal""")
+                
+                opcio_menu_02 = comprobarInt(text = "\nTria entre agregar/eliminar una dada d'un alumne o tornar al menú principal: ")
 
-            match opcio_menu_02:
-                case 1:
-                    while True:
-                        LlistaAlumnes(alumnes)
-                        index_alumne_agregar = comprobarIndexLlista(alumnes, text = "\nTria l'index al que vols agregar una dada: ")
-                        for index_alumne, alumne in enumerate (alumnes):
-                            if index_alumne == index_alumne_agregar:
-                                print(f"Les dades de l'alumne amb index {index_alumne_agregar} son:")
-                            for index_dada, campo in enumerate (alumne):
+                match opcio_menu_02:
+
+                    # Agrega una dada nova a partir de l'index de l'alumne que l'usuari indica
+                    case 1:
+                        while True:
+                            LlistaAlumnes(alumnes)
+                            index_alumne_agregar = comprovarIndexLlista(comprovar_index = alumnes, text = "\nTria l'index al que vols agregar una dada: ")
+                            for index_alumne, alumne in enumerate (alumnes):
                                 if index_alumne == index_alumne_agregar:
-                                    print(F"Index: {index_dada}: {campo}")
-                        
-                        dada_nova = comprobarStr(dada = "nova")
-
-                        alumnes[index_alumne_agregar].append(dada_nova)
-                        print("\nDada agregada correctament")
-
-                        while True:
-                            agregar_altre_dada = input("Vols agregar una altra dada (Si/No)? ").lower()
-                            if agregar_altre_dada != "si" and agregar_altre_dada != "no":
-                                print("Resposta incorrecta, torna-ho a provar")
-                            else:
-                                break
+                                    print(f"Les dades de l'alumne amb index {index_alumne_agregar} son:")
+                                for index_dada, campo in enumerate (alumne):
+                                    if index_alumne == index_alumne_agregar:
+                                        print(F"Index: {index_dada}: {campo}")
                             
-                        if agregar_altre_dada == "si":
-                            continue
-                        else:
-                            print("Tornant al menú principal..")
-                            break
+                            dada_nova = comprobarStr(dada = "nova")
 
-                case 2:
-                    while True:
-                        LlistaAlumnes(alumnes)
-                        index_alumne_eliminar = comprobarIndexLlista(alumnes, text = "\nTria l'index que té la dada que vols eliminar: ")
-                        for index_alumne, alumne in enumerate (alumnes):
-                            if index_alumne == index_alumne_eliminar:
-                                print(f"Les dades de l'alumne amb index {index_alumne_eliminar} son:")
-                            for index_dada, campo in enumerate (alumne):
+                            alumnes[index_alumne_agregar].append(dada_nova)
+                            print("\nDada agregada correctament")
+
+                            while True:
+                                agregar_altre_dada = input("Vols agregar una altra dada (Si/No)? ").lower()
+                                if agregar_altre_dada != "si" and agregar_altre_dada != "no":
+                                    print("Resposta incorrecta, torna-ho a provar")
+                                else:
+                                    break
+                                
+                            if agregar_altre_dada == "si":
+                                continue
+                            else:
+                                print("Sortint de l'agregador de dades..")
+                                break
+
+                    # Elimina una dada a partir de l'index de l'alumne i de la dada que l'usuari indica
+                    case 2:
+                        while True:
+                            LlistaAlumnes(alumnes)
+                            index_alumne_eliminar = comprovarIndexLlista(comprovar_index = alumnes, text = "\nTria l'index que té la dada que vols eliminar: ")
+                            for index_alumne, alumne in enumerate (alumnes):
                                 if index_alumne == index_alumne_eliminar:
-                                    print(F"Index: {index_dada}: {campo}")
-                                    
-                        index_dada_eliminar = comprobarIndexLlista(alumnes, text = "\nIndica l'index de la dada que vols eliminar: ")
+                                    print(f"Les dades de l'alumne amb index {index_alumne_eliminar} son:")
+                                for index_dada, campo in enumerate (alumne):
+                                    if index_alumne == index_alumne_eliminar:
+                                        print(F"Index: {index_dada}: {campo}")
+                                        
+                            index_dada_eliminar = comprovarIndexLlista(comprovar_index = alumnes[index_alumne_eliminar], text = "\nIndica l'index de la dada que vols eliminar: ")
 
-                        del alumnes[index_alumne_eliminar][index_dada_eliminar]
-                        print("\nDada eliminada correctament")
+                            del alumnes[index_alumne_eliminar][index_dada_eliminar]
+                            print("\nDada eliminada correctament")
 
-                        while True:
-                            eliminar_altre_dada = input("Vols eliminar un altre dada (Si/No)? ").lower()
-                            if eliminar_altre_dada != "si" and eliminar_altre_dada != "no":
-                                print("Resposta incorrecta, torna-ho a provar")
+                            while True:
+                                eliminar_altre_dada = input("Vols eliminar un altre dada (Si/No)? ").lower()
+                                if eliminar_altre_dada != "si" and eliminar_altre_dada != "no":
+                                    print("Resposta incorrecta, torna-ho a provar")
+                                else:
+                                    break
+                                
+                            if eliminar_altre_dada == "si":
+                                continue
                             else:
+                                print("Sortint de l'eliminador de dades..")
                                 break
-                            
-                        if eliminar_altre_dada == "si":
-                            continue
-                        else:
-                            print("Tornant al menú principal..")
-                            break
-                case _:
-                    print("Opció incorrecta, tria una opció dins del rang")
+                    case 3:
+                        print("Tornant al menú principal..")
+                        break
 
+                    case _:
+                        print("Opció incorrecta, tria una opció dins del rang")
+
+        # Surt del programa
         case 6:
             print("Sortint de la base de dades dels alumnes..")
             break
-
+        
+        # Controla que l'usuari introdueixi una opció correcta
         case _:
             print("Opció incorrecta, tria una opció dins del rang")
